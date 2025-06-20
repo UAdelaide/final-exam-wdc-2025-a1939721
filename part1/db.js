01a21f84c745b0d1cc17762ab83ce454bab3e0d8
Natsumi Baba <a1939721@adelaide.edu.au>
2025-06-20 05:41:23 +0000

    [automatic commit] 2025-06-20 05:41:23 UTC 113.197.13.138

--- part1/db.js
+++ part1/db.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-    host: 'localhost',
+    socketPath: 
+    host: '127.0.0.1',
     user: 'root',
     password: 'mypassword',
     database: 'DogWalkService'
