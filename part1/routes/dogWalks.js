7bc2ceeb9f44e0c02e688d0651437948f4360df2
Natsumi Baba <a1939721@adelaide.edu.au>
2025-06-20 06:41:08 +0000

    [automatic commit] 2025-06-20 06:41:08 UTC 113.197.13.138

--- part1/routes/dogWalks.js
+++ part1/routes/dogWalks.js
@@ -47,7 +47,7 @@ router.get('/walkers/summary', async (req, res) => {
             SELECT
             u.username AS walker_username,
             COUNT(r.rating_id) AS total_ratings,
-            AVG(wRate.rating)wr.average_rating,
+            AVG(r.rating) AS average_rating
             wr.completed_walks,
             FROM WalkRatings wRate
             JOIN Users u ON d.owner_id = u.user_id
