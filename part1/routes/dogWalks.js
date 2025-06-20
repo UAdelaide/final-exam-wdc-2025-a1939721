f6ad08cbba506e7e5a25c7c212251dd4ef2b46bb
Natsumi Baba <a1939721@adelaide.edu.au>
2025-06-20 06:18:07 +0000

    [automatic commit] 2025-06-20 06:18:07 UTC 113.197.13.138

--- part1/routes/dogWalks.js
+++ part1/routes/dogWalks.js
@@ -30,10 +30,9 @@ router.get('/walkrequests/open', async (req, res) => {
             wr.location,
             u.username As owner_username
             FROM WalkRequests wr
-            JOIN
-            Dogs d ON wr.dog_id = d.dog_id
-            Users u ON d.owner_id = u.user_id
-            WHERE wr.status = 'open'
+            JOIN Dogs d ON wr.dog_id = d.dog_id
+            JOIN Users u ON d.owner_id = u.user_id
+            WHERE wr.status = 'open's
             `);
             res.json(rows);
     } catch(err){
