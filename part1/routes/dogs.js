a74e28e8f5201b1182b0e89a5461c5da00cc97ad
Natsumi Baba <a1939721@adelaide.edu.au>
2025-06-20 05:36:56 +0000

    [automatic commit] 2025-06-20 05:36:55 UTC 113.197.13.138

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -12,7 +12,7 @@ router.get('/dogs', async (req, res) => {
             res.json(rows);
     } catch(err){
         console.error('Error fetching data');
-        res.status(500).json({error:'Server error'});
+        res.status(500).json({ error:'Server error'});
     }
 });
 
