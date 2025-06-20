fe8c71a9559e397d2350e948bb87943cd59e371b
Natsumi Baba <a1939721@adelaide.edu.au>
2025-06-20 05:10:12 +0000

    [automatic commit] 2025-06-20 05:10:12 UTC 113.197.13.138

--- part1/routes/dogWalk.js
+++ part1/routes/dogWalk.js
@@ -6,10 +6,11 @@ router.get('/dogs', async (req, res) => {
     try {
         const [rows] = await db.query(`
             SELECT d.dog_id, d.name as DogName,
+            
             `);
             res.json(rows);
     } catch(err){
-        console.error('Error fetching data')
+        console.error('Error fetching data');
         res.status(500).json({error:'Server error'});
     }
 });
