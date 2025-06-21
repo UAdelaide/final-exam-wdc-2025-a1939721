11acefc3cff75fcc91549dd9640c1fde7677a456
Natsumi Baba <a1939721@adelaide.edu.au>
2025-06-21 05:27:03 +0000

    [automatic commit] 2025-06-21 05:27:03 UTC 14.2.23.199

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -64,12 +64,12 @@ router.post('/login', async (req, res) => {
 });
 
 // logout
-router.post('/logout', (req, res){
+router.post('/logout', (req, res) => {
   req.session.destroy(err => {
     if(err){
       return res.status(500).json({'Logout failed'});
     }
   })
-}
+});
 
 module.exports = router;
