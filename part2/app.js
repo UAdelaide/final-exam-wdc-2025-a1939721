45cbf509a92086e7c311ae98e893a490c8ee0c32
Natsumi Baba <a1939721@adelaide.edu.au>
2025-06-21 03:20:37 +0000

    [automatic commit] 2025-06-21 03:20:37 UTC 14.2.23.199

--- part2/app.js
+++ part2/app.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(
     session({
-        secret: process.env.SESSION_SECRET || 'dogwalk-secret',
+        secret: process.env.SESSION_SECRET || '',
         resave: false,
         saveUninitialized: false,
         cookie: {httpOnly: true, maxAge: 1000 * 60 * 60}
