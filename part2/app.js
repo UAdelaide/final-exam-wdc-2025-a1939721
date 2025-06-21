437b6ff5a43de48ca98b5511c22f3f7f1013585d
Natsumi Baba <a1939721@adelaide.edu.au>
2025-06-21 06:32:16 +0000

    [automatic commit] 2025-06-21 06:32:16 UTC 14.2.23.199

--- part2/app.js
+++ part2/app.js
@@ -26,6 +26,7 @@ const dogRouter = require('./routes/dog');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api', dogRouter);
+app.use('/api', dogRouter);
 
 // Export the app instead of listening here
 module.exports = app;
