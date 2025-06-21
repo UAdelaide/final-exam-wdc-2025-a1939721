e4c5b004353692ee0cdef45fbf012155b1d3389d
Natsumi Baba <a1939721@adelaide.edu.au>
2025-06-21 00:28:46 +0000

    [automatic commit] 2025-06-21 00:28:46 UTC 14.2.23.199

--- part2/routes/login.js
+++ part2/routes/login.js
@@ -7,7 +7,7 @@ router.post('/login', async (req, res) =>{
     const { username, password } = req.body;
 
     try{
-        const [rows] = awaitdb.query(`
+        const [rows] = await db.query(`
         SELECT role FROM Users
         WHERE username = ? AND password_hash = ?
         `,
