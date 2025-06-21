e377df7757af736dba2ca81b7e4fabb00283dfd1
Natsumi Baba <a1939721@adelaide.edu.au>
2025-06-21 03:47:22 +0000

    [automatic commit] 2025-06-21 03:47:22 UTC 14.2.23.199

--- part2/routes/dog.js
+++ part2/routes/dog.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../db');
+var db = require('../models/db');
 
 router.get('/dogs', async (req, res) => {
     try {
