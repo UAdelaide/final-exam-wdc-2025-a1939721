64d548dc485cc8321c628b02a56be16948ae184c
Natsumi Baba <a1939721@adelaide.edu.au>
2025-06-21 06:11:46 +0000

    [automatic commit] 2025-06-21 06:11:46 UTC 14.2.23.199

--- part2/routes/dog.js
+++ part2/routes/dog.js
@@ -21,6 +21,24 @@ router.get('/dogs', async (req, res) => {
     }
 });
 
+router.get('/mydogs', async (req, res) => {
+    try {
+        const [rows] = await db.query(`
+            SELECT
+            d.dog_id,
+            d.name,
+            d.size,
+            d.owner_id,
+            u.username AS owner_username
+            FROM Dogs d
+            JOIN Users u ON d.owner_id = u.user_id
+            `);
+            res.json(rows);
+    } catch(err){
+        console.error('Error fetching data', err);
+        res.status(500).json({error:'Server error'});
+    }
+});
 
 
 module.exports = router;
